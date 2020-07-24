f6ef30b0ce788e91cada20fa63d3c37480492585
sloothes <sloothes.com@gmail.com>
2020-07-24 20:31:23 +0300

    toggle buttons

--- src/skydome-tab-ui.js
+++ src/skydome-tab-ui.js
@@ -143,7 +143,7 @@
 	//	var tab = TabUI.Skydome.tab;
 
 		var row = document.createElement("h3");
-		row.style.cssText = "height:30px;margin-bottom:20px;"
+		row.style.cssText = "height:40px;margin-bottom:20px;"
 
 		var button = document.createElement("div");
 		button.id = "skydome-upload-button";
@@ -162,4 +162,23 @@
 
 	})( TabUI.Skydome.tab );
 
+//	ground-toggle-ui.js
 
+	(function( tab ){
+
+	//	Ground toggle button.
+	//	var tab = TabUI.Skydome.tab;
+
+		var row = document.createElement("h3");
+		row.style.cssText = "height:30px;margin-bottom:20px;"
+
+		var button = document.createElement("div");
+		button.id = "ground-toggle-button";
+		button.textContent = "Toggle Ground Helper";
+		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
+		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
+
+		row.appendChild( button );
+		tab.appendChild( row );
+
+	})( TabUI.Skydome.tab );
