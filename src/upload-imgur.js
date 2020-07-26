b68320082a1f35731ab5c360c8e06c9e72004d11
sloothes <sloothes.com@gmail.com>
2020-07-26 12:25:36 +0300

    skyboxes background

--- src/upload-imgur.js
+++ src/upload-imgur.js
@@ -1,3 +1,115 @@
+    function uploadDataURL(data, type, name){
+
+    //  Returns a resolved promise with record data from imgur.com.
+        debugMode && console.log("uploading:", file.name);
+        return new Promise(function( resolve, reject ){
+
+            var formdata = new FormData();
+            formdata.append("image", data);
+            formdata.append("type",  type);
+            formdata.append("name",  name);
+
+        //	var endpoint = "https://api.imgur.com/3/image";
+        //	var clientID = "06217f601180652";  // sloothes app Client-ID.
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("POST", endpoint, true);
+            xhttp.setRequestHeader("Authorization", "Client-ID " + clientID);
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    var response = "";
+                    if (this.status >= 200 && this.status < 300) {
+                        response = JSON.parse(this.responseText);
+                        debugMode && console.log(response);
+                        resolve(response.data); // resolve promise.
+                    } else {
+                        var err = JSON.parse(this.responseText).data.error;
+                        console.error( err.type, err );
+                        reject( err );
+                    }
+                }
+            };
+
+            xhttp.send(formdata);
+            xhttp = null;
+        });
+
+    };
+
+    function uploadImageFile(file){
+
+    //  Returns a resolved promise with record data from imgur.com.
+        debugMode && console.log("uploading:", file);
+        return new Promise(function( resolve, reject ){
+
+            var formdata = new FormData();
+            formdata.append("image", file);
+            formdata.append("type",  file.type);
+            formdata.append("name",  file.name);
+
+        //	var endpoint = "https://api.imgur.com/3/image";
+        //	var clientID = "06217f601180652";  // sloothes app Client-ID.
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("POST", endpoint, true);
+            xhttp.setRequestHeader("Authorization", "Client-ID " + clientID);
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    var response = "";
+                    if (this.status >= 200 && this.status < 300) {
+                        response = JSON.parse(this.responseText);
+                    //	debugMode && console.log(response.data);
+                        resolve(response.data); // resolve promise.
+                    } else {
+                        var err = JSON.parse(this.responseText).data.error;
+                        console.error( err.type, err );
+                        reject( err );
+                    }
+                }
+            };
+
+            xhttp.send(formdata);
+            xhttp = null;
+        });
+
+    };
+
+    function deleteImage(deletehash){
+
+    //  Returns a resolved promise with success response if from api.imgur.com.
+        debugMode && console.log("DELETE https://api.imgur.com/3/image/" + deletehash);
+        return new Promise(function( resolve, reject ){
+
+        //	var endpoint = "https://api.imgur.com/3/image/" + deletehash;
+        //	var clientID = "06217f601180652";  // sloothes app Client-ID.
+
+            var xhttp = new XMLHttpRequest();
+            var endpoint = endpoint + "/" + deletehash;
+            xhttp.open("DELETE", endpoint, true);
+            xhttp.setRequestHeader("Authorization", "Client-ID " + clientID);
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4) {
+                    var response = "";
+                    if (this.status >= 200 && this.status < 300) {
+                        response = JSON.parse(this.responseText);
+                        debugMode && console.log(response);
+                        resolve(response); // resolve promise.
+                    } else {
+                        var err = JSON.parse(this.responseText).data.error;
+                        console.error( err.type, err );
+                        reject( err );
+                    }
+                }
+            };
+
+            xhttp.send();
+            xhttp = null;
+        });
+
+    };
+
+
+
 //  upload-imgur.js
 
     function uploadToImgur(file, callback){
