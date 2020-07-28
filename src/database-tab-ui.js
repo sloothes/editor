7b3bb61a8b6574d5ae1f5c1d8fffc3bad2ff400d
sloothes <sloothes.com@gmail.com>
2020-07-28 08:24:59 +0300

    database alpha v0.1.6

--- src/database-tab-ui.js
+++ src/database-tab-ui.js
@@ -312,3 +312,24 @@
 
 	})( TabUI.Database.tab );
 
+//	load-object-ui.js
+
+	(function( tab ){
+
+	//	Document Load button.
+	//	var tab = TabUI.Database.tab;
+
+		var row = document.createElement("h3");
+		row.style.cssText = "height:30px;margin-bottom:20px;"
+
+		var button = document.createElement("div");
+		button.id = "doc-load-button";
+		button.textContent = "Load document";
+		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
+		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
+
+		row.appendChild( button );
+		tab.appendChild( row );
+
+	})( TabUI.Database.tab );
+
