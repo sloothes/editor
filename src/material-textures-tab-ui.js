ff344857f7eeafa9d223a90fe11d98cf5a50c785
sloothes <sloothes.com@gmail.com>
2020-08-04 17:18:29 +0300

    toggle grid button

--- src/material-textures-tab-ui.js
+++ src/material-textures-tab-ui.js
@@ -253,6 +253,19 @@
 
 	})( TabUI.MaterialTextures.tab );
 
+	(function( tab ){
+
+	//	MaterialTextures Grid Toggle button.
+
+		var row = document.createElement("h3"); row.style.cssText = "height:30px;margin-bottom:20px;"
+		var button = document.createElement("div"); 
+		button.id = "material-textures-viewer-grid"; button.textContent = "Toggle Viewer Grid";
+		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
+		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
+		row.appendChild( button ); tab.appendChild( row );
+
+	})( TabUI.MaterialTextures.tab );
+
 //	material-textures-exit-editor-ui.js
 
 	(function( tab ){
