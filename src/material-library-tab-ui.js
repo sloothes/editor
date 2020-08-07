1b95ac1fe38ae4c606188eb7ae4f3867af457250
sloothes <sloothes.com@gmail.com>
2020-08-07 04:23:25 +0300

    Update material-library-tab-ui.js

--- src/material-library-tab-ui.js
+++ src/material-library-tab-ui.js
@@ -78,7 +78,7 @@
 		var row = document.createElement("h3"); row.style.cssText = "height:30px;margin-bottom:20px;";
 
 		var button = document.createElement("div"); 
-		button.id = "material-library-load-button"; button.textContent = "Load Selected Material";
+		button.id = "material-library-load-button"; button.textContent = "Load Material";
 		button.classList.add( "form-control","btn", "btn-primary","btn-white-outline","gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;";
 		button.style.cssText += "font-size:large;margin-left:15px;margin-right:15px;";
