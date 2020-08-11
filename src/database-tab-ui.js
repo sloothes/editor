e21f8e8fba1a23d2a3937ae9e60184fe65faa79d
sloothes <sloothes.com@gmail.com>
2020-08-11 09:56:10 +0300

    Update database-tab-ui.js

--- src/database-tab-ui.js
+++ src/database-tab-ui.js
@@ -76,12 +76,9 @@
 
 		const dummy_src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
 
-		var row = document.createElement("h3");
-		row.style.cssText = "height:260px;border:none;text-align:center;margin-right:15px;";
+		var row = document.createElement("h3"); row.style.cssText = "height:260px;border:none;text-align:center;margin-right:15px;";
 
-		var img = new Image(256,256)
-		img.src = dummy_src;
-		img.id = "image-url-viewer";
+		var img = new Image(256,256); img.src = dummy_src; img.id = "image-url-viewer";
 		img.style.cssText = "width:256px;height:256px;margin:auto;background-repeat:repeat;";
 		img.style.cssText += "background-image:url('https://i.imgur.com/rnZZU0i.png') !important;";
 
@@ -111,19 +108,12 @@
 	(function( tab ){
 
 	//	Image upload button.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.style.cssText = "height:30px;margin-bottom:20px;"
-
-		var button = document.createElement("div");
-		button.id = "upload-image-data";
-		button.textContent = "Upload Image Data";
+		var row = document.createElement("h3"); row.style.cssText = "height:30px;margin-bottom:20px;"
+		var button = document.createElement("div"); button.id = "upload-image-data"; button.textContent = "Upload Image Data";
 		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
-
-		row.appendChild( button );
-		tab.appendChild( row );
+		row.appendChild( button ); tab.appendChild( row );
 
 	})( TabUI.Database.tab );
 
@@ -132,19 +122,12 @@
 	(function( tab ){
 
 	//	Textures cache button.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.style.cssText = "height:40px;margin-bottom:20px;"
-
-		var button = document.createElement("div");
-		button.id = "cache-textures-button";
-		button.textContent = "Cache Texture Images";
+		var row = document.createElement("h3");row.style.cssText = "height:40px;margin-bottom:20px;display:none;"
+		var button = document.createElement("div"); button.id = "cache-textures-button"; button.textContent = "Cache Texture Images";
 		button.classList.add( "form-control","btn", "btn-primary","btn-white-outline","gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
-
-		row.appendChild( button );
-		tab.appendChild( row );
+		row.appendChild( button ); tab.appendChild( row );
 
 	})( TabUI.Database.tab );
 
@@ -153,26 +136,56 @@
 	(function( tab ){
 
 	//	Undo/Redo button.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.style.cssText = "display:none;height:40px;margin-bottom:20px;"
+		var row = document.createElement("h3"); row.style.cssText = "display:none;height:40px;margin-bottom:20px;"
 
-		var redo = document.createElement("div");
-		redo.id = "database-redo-button";
-		redo.textContent = "Redo";
+		var redo = document.createElement("div"); redo.id = "database-redo-button"; redo.textContent = "Redo";
 		redo.style.cssText = "width:44%;float:left;height:40px;font-size:large;margin-right:15px;";
 		redo.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 
-		var undo = document.createElement("div");
-		undo.id = "database-undo-button";
-		undo.textContent = "Undo";
+		var undo = document.createElement("div"); undo.id = "database-undo-button"; undo.textContent = "Undo";
 		undo.style.cssText = "width:44%;float:right;height:40px;font-size:large;margin-right:15px;";
 		undo.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 
-		row.appendChild( redo );
-		row.appendChild( undo );
+		row.appendChild( redo ); row.appendChild( undo ); tab.appendChild( row );
+
+	})( TabUI.Database.tab );
+
+//	find-droplist-ui.js
+
+	(function( tab ){
+
+	//	Document Find droplist.
+
+		var row = document.createElement("h3"); row.textContent = "key:"; row.style.cssText = "height:40px;margin:10px 0px;";
+
+		var select = document.createElement("select"); select.id = "doc-find-droplist";
+		select.style.cssText = "width:200px;color:#000;float:right;border:1px solid;border-radius:4px;";
+		select.style.cssText += "padding:2px 4px 4px 4px;font-size:20px;margin-left:10px;margin-right:15px;";
+
+		var keys = "_id,name,type,uuid,children";
+
+		keys.split(",").forEach(function( name ){
+			var option = document.createElement("option");
+			option.text = name; option.value = name;
+			select.appendChild( option );
+		});
+
+		row.appendChild( select );
 		tab.appendChild( row );
+
+	})( TabUI.Database.tab );
+
+	(function( tab ){
+
+	//	Document find input.
+
+		var row = document.createElement("h3"); row.style.cssText = "height:30px;text-align:center;margin:10px 15px;";
+		var input = document.createElement("input"); input.id = "doc-find-input";
+		input.setAttribute("placeholder", "name" ); input.classList.add("form-control","text-center");
+		input.style.cssText = "color:#000;border:none;display:inline;width:180px;margin:0px 10px 0px 10px;";
+		input.style.cssText += "text-align:center;font-size:large;font-weigth:bold;background:none;";
+		row.appendChild(input); tab.appendChild( row );
 
 	})( TabUI.Database.tab );
 
@@ -181,39 +194,46 @@
 	(function( tab ){
 
 	//	Document skip input.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.textContent = "skip:";
-		row.style.cssText = "height:40px;margin:30px 15px 20px 0;";
+		var row = document.createElement("h3"); 
+		row.textContent = "skip:"; row.style.cssText = "height:40px;margin:30px 15px 20px 0;";
 
-		var vect = document.createElement("div");
-		vect.style.cssText = "width:200px;height:40px;float:right;";
+		var vect = document.createElement("div"); vect.style.cssText = "width:200px;height:40px;float:right;";
+		var prev = document.createElement("li"); prev.id = "doc-skip-decrease"; prev.innerHTML = "&#9668;";
+		prev.style.display = "inline"; prev.classList.add("btn","btn-primary","get-prev-btn","pull-left");
+		var next = document.createElement("li"); next.id = "doc-skip-increase"; next.innerHTML = "&#9658;";
+		next.style.display = "inline"; next.classList.add("btn","btn-primary","get-next-btn","pull-right");
 
-		var prev = document.createElement("li");
-		prev.id = "doc-skip-decrease";
-		prev.innerHTML = "&#9668;";
-		prev.style.display = "inline";
-		prev.classList.add("btn","btn-primary","get-prev-btn","pull-left");
+		var input = document.createElement("input"); input.id = "doc-skip-input";
+		input.setAttribute("placeholder", "number" ); input.classList.add("form-control","text-center");
+		input.style.cssText = "color:#000;border:none;display:inline;width:110px;";
+		input.style.cssText += "margin:0px 5px;text-align:center;font-size:large;font-weigth:bold;background:none;";
 
-		var next = document.createElement("li");
-		next.id = "doc-skip-increase";
-		next.innerHTML = "&#9658;";
-		next.style.display = "inline";
-		next.classList.add("btn","btn-primary","get-next-btn","pull-right");
+		vect.appendChild(prev); vect.appendChild(input); vect.appendChild(next); row.appendChild(vect); tab.appendChild( row );
 
-		var input = document.createElement("input");
-		input.id = "doc-skip-input";
-		input.setAttribute("placeholder", "number" );
-		input.classList.add("form-control","text-center");
-		input.style.cssText = "color:#000;border:none;display:inline;width:110px;"
-		+ "margin:0px 5px;text-align:center;font-size:large;font-weigth:bold;background:none;";
+	})( TabUI.Database.tab );
 
-		vect.appendChild(prev);
-		vect.appendChild(input);
-		vect.appendChild(next);
-		row.appendChild(vect);
-		tab.appendChild( row );
+//	limit-input-ui.js
+
+	(function( tab ){
+
+	//	Document skip input.
+
+		var row = document.createElement("h3"); 
+		row.textContent = "skip:"; row.style.cssText = "height:40px;margin:30px 15px 20px 0;";
+
+		var vect = document.createElement("div"); vect.style.cssText = "width:200px;height:40px;float:right;";
+		var prev = document.createElement("li"); prev.id = "doc-limit-decrease"; prev.innerHTML = "&#9668;";
+		prev.style.display = "inline"; prev.classList.add("btn","btn-primary","get-prev-btn","pull-left");
+		var next = document.createElement("li"); next.id = "doc-limit-increase"; next.innerHTML = "&#9658;";
+		next.style.display = "inline"; next.classList.add("btn","btn-primary","get-next-btn","pull-right");
+
+		var input = document.createElement("input"); input.id = "doc-limit-input";
+		input.setAttribute("placeholder", "number" ); input.classList.add("form-control","text-center");
+		input.style.cssText = "color:#000;border:none;display:inline;width:110px;";
+		input.style.cssText += "margin:0px 5px;text-align:center;font-size:large;font-weigth:bold;background:none;";
+
+		vect.appendChild(prev); vect.appendChild(input); vect.appendChild(next); row.appendChild(vect); tab.appendChild( row );
 
 	})( TabUI.Database.tab );
 
@@ -222,19 +242,12 @@
 	(function( tab ){
 
 	//	Document Load button.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.style.cssText = "height:40px;margin-bottom:20px;"
-
-		var button = document.createElement("div");
-		button.id = "doc-load-button";
-		button.textContent = "Load document";
+		var row = document.createElement("h3"); row.style.cssText = "height:40px;margin-bottom:20px;"
+		var button = document.createElement("div"); button.id = "doc-load-button"; button.textContent = "Load document";
 		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
-
-		row.appendChild( button );
-		tab.appendChild( row );
+		row.appendChild( button ); tab.appendChild( row );
 
 	})( TabUI.Database.tab );
 
@@ -243,17 +256,12 @@
 	(function( tab ){
 
 	//	Document Keys droplist.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.textContent = "key:";
-		row.style.cssText = "height:40px;margin:10px 0px;";
+		var row = document.createElement("h3"); row.textContent = "key:"; row.style.cssText = "height:40px;margin:10px 0px;";
 
-		var select = document.createElement("select");
-		select.id = "doc-keys-droplist";
-		select.style.cssText = "width:200px;color:#000;float:right;"
-		+ "border:1px solid;border-radius:4px;padding:2px 4px 4px 4px;"
-		+ "font-size:20px;margin-left:10px;margin-right:15px;";
+		var select = document.createElement("select"); select.id = "doc-keys-droplist";
+		select.style.cssText = "width:200px;color:#000;float:right;border:1px solid;border-radius:4px;";
+		select.style.cssText += "padding:2px 4px 4px 4px;font-size:20px;margin-left:10px;margin-right:15px;";
 
 		var keys = ",";
 
@@ -274,7 +282,6 @@
 	(function( tab ){
 
 	//	Document Value input.
-	//	var tab = TabUI.Database.tab;
 
 		var row = document.createElement("h3");
 		row.textContent = "value:";
@@ -315,19 +322,12 @@
 	(function( tab ){
 
 	//	Value Update button.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.style.cssText = "height:30px;margin-bottom:20px;"
-
-		var button = document.createElement("div");
-		button.id = "update-value-button";
-		button.textContent = "Update Value";
+		var row = document.createElement("h3"); row.style.cssText = "height:30px;margin-bottom:20px;"
+		var button = document.createElement("div"); button.id = "update-value-button"; button.textContent = "Update Value";
 		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
-
-		row.appendChild( button );
-		tab.appendChild( row );
+		row.appendChild( button ); tab.appendChild( row );
 
 	})( TabUI.Database.tab );
 
@@ -336,19 +336,12 @@
 	(function( tab ){
 
 	//	Document Update button.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.style.cssText = "height:30px;margin-bottom:20px;"
-
-		var button = document.createElement("div");
-		button.id = "update-doc-button";
-		button.textContent = "Update document";
+		var row = document.createElement("h3"); row.style.cssText = "height:30px;margin-bottom:20px;"
+		var button = document.createElement("div"); button.id = "update-doc-button"; button.textContent = "Update document";
 		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
-
-		row.appendChild( button );
-		tab.appendChild( row );
+		row.appendChild( button ); tab.appendChild( row );
 
 	})( TabUI.Database.tab );
 
@@ -357,19 +350,12 @@
 	(function( tab ){
 
 	//	Document Remove button.
-	//	var tab = TabUI.Database.tab;
 
-		var row = document.createElement("h3");
-		row.style.cssText = "height:30px;margin-bottom:20px;"
-
-		var button = document.createElement("div");
-		button.id = "doc-remove-button";
-		button.textContent = "Remove document";
+		var row = document.createElement("h3"); row.style.cssText = "height:30px;margin-bottom:20px;"
+		var button = document.createElement("div"); button.id = "doc-remove-button"; button.textContent = "Remove document";
 		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
-
-		row.appendChild( button );
-		tab.appendChild( row );
+		row.appendChild( button ); tab.appendChild( row );
 
 	})( TabUI.Database.tab );
 
