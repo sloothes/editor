4e8f40375012167668e059927d1acd02ced96194
sloothes <sloothes.com@gmail.com>
2020-08-12 01:54:26 +0300

    Update model3D-tab-ui.js

--- src/model3D-tab-ui.js
+++ src/model3D-tab-ui.js
@@ -1,6 +1,6 @@
 //	model3D-tab-ui.js
 
-	TabUI.add( "ModelLoader", "model-3d-tab" ); TabUI.append( "ModelLoader" ); TabUI.ModelLoader.pill.innerHTML = "Model&nbsp;Loader";
+	TabUI.add( "ModelLoader", "model-loader-tab" ); TabUI.append( "ModelLoader" ); TabUI.ModelLoader.pill.innerHTML = "Model&nbsp;Loader";
 
 //	model3D-collection-droplist-ui.js
 
@@ -11,7 +11,7 @@
 		var row = document.createElement("h3"); 
 		row.textContent = "Collection:"; row.style.cssText = "height:30px;"
 
-		var select = document.createElement("select"); select.id = "model-3d-collection-droplist";
+		var select = document.createElement("select"); select.id = "model-loader-collection-droplist";
 		select.style.cssText = "width:150px;color:#000;float:right;border:1px solid;border-radius:4px;";
 		select.style.cssText += "padding:2px 4px 4px 4px;font-size:20px;margin-left:10px;margin-right:15px;";
 
@@ -35,11 +35,11 @@
 		const dummy_src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
 		var row = document.createElement("h3"); row.style.cssText = "height:260px;border:none;text-align:center;"; // margin-right:15px;
 
-		var canvas = document.createElement("canvas"); canvas.width = 256; canvas.height = 256; canvas.id = "model-3d-viewer";
+		var canvas = document.createElement("canvas"); canvas.width = 256; canvas.height = 256; canvas.id = "model-loader-viewer";
 		canvas.style.cssText = "width:256px;height:256px;margin:auto;";
 	//	canvas.style.cssText += "background-repeat:repeat;background-image:url('https://i.imgur.com/rnZZU0i.png') !important;";
 
-		var img = new Image(256,256); img.id = "model-3d-image-viewer"; img.src = dummy_src; img.style.cssText = "margin:0 30px;display:none;"; 
+		var img = new Image(256,256); img.id = "model-loader-image-viewer"; img.src = dummy_src; img.style.cssText = "margin:0 30px;display:none;"; 
 		watch( img, "onload",  function(prop, event, value){ canvas.getContext( "2d" ).drawImage( img, 0, 0, canvas.width, canvas.height ); });
 		watch( img, "onerror", function(prop, event, value){ canvas.getContext( "2d" ).clearRect( 0, 0, canvas.width, canvas.height ); });
 		img.addEventListener("load",function(event){ callWatchers( this, "onload", "load", this.src ); })
@@ -58,15 +58,15 @@
 
 		var row = document.createElement("h3"); row.style.cssText = "height:30px;text-align:center;margin:10px 15px;";
 
-		var prev = document.createElement("li"); prev.id = "model-3d-doc-previous";
+		var prev = document.createElement("li"); prev.id = "model-loader-doc-previous";
 		prev.innerHTML = "&#9668;"; prev.style.display = "inline";
 		prev.classList.add("btn","btn-primary","get-prev-btn","pull-left");
 
-		var next = document.createElement("li"); next.id = "model-3d-doc-next";
+		var next = document.createElement("li"); next.id = "model-loader-doc-next";
 		next.innerHTML = "&#9658;"; next.style.display = "inline";
 		next.classList.add("btn","btn-primary","get-next-btn","pull-right");
 
-		var input = document.createElement("input"); input.id = "model-3d-name-input";
+		var input = document.createElement("input"); input.id = "model-loader-name-input";
 		input.setAttribute("placeholder", "name" ); input.classList.add("form-control","text-center");
 		input.style.cssText = "color:#000;border:none;display:inline;width:170px;"; // margin:0px 10px 0px 10px;
 		input.style.cssText += "text-align:center;font-size:large;font-weigth:bold;background:none;";
@@ -82,7 +82,7 @@
 	//	model3D Load button.
 
 		var row = document.createElement("h3"); row.style.cssText = "height:40px;margin-bottom:20px;"
-		var button = document.createElement("div"); button.id = "model-3d-load-button"; 
+		var button = document.createElement("div"); button.id = "model-loader-load-button"; 
 		button.textContent = "Load Model"; button.classList.add( "form-control","btn", "btn-primary","btn-white-outline","gradient-btn" );
 		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-left:15px;margin-right:15px;";
 
