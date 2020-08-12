f7dbfd46fae2b249a1e1d25d5baeb4b0ca485f29
sloothes <sloothes.com@gmail.com>
2020-08-12 07:02:58 +0300

    Update model-loader-tab-ui.js

--- src/model-loader-tab-ui.js
+++ src/model-loader-tab-ui.js
@@ -117,7 +117,7 @@
 		var row = document.createElement("h3"); row.style.cssText = "height:30px;margin-bottom:20px;"
 		var button = document.createElement("div"); button.id = "model-loader-apply-matrix"; button.textContent = "Apply Model Matrix";
 		button.classList.add( "form-control", "btn", "btn-primary", "btn-white-outline", "gradient-btn" );
-		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-right:15px;";
+		button.style.cssText = "width:-webkit-fill-available;float:right;height:40px;font-size:large;margin-left:15px;margin-right:15px;";
 		row.appendChild( button ); tab.appendChild( row );
 
 	})( TabUI.ModelLoader.tab );
